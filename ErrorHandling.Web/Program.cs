ec84831ed4324d6a7746104b81433792496531c1
davarng <113583854+davarng@users.noreply.github.com>
2025-05-22 11:06:19 +0200

    added controllers in program file

--- ErrorHandling.Web/Program.cs
+++ ErrorHandling.Web/Program.cs
@@ -5,9 +5,10 @@ namespace ErrorHandling.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapControllers();
 
             app.Run();
         }
